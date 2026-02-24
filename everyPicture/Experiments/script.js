1524cfa19d9c083a3b7a03b2a6d7be976a3cfb0d
“Anjali <aaravi@ucdavis.edu>
2026-02-24 08:18:04 -0800

    blah

--- everyPicture/Experiments/script.js
+++ everyPicture/Experiments/script.js
@@ -4,8 +4,11 @@
 
     const myImages = ['ear1.jpg', 'ear2.jpg', 'ear3.jpg', 'ear4.jpg', 'ear5.jpg','ear6.jpg', 'ear7.jpg'];
 
+    const myInfomation = ['I got my helix piercing on the left side of my ear when I was 15 years old. I wanted it because I had just seen the Black Widow movie where the character Yelana (who I was obsessed with) had a helix piercing.', 'caption for ear 2', 'caption for ear 3'];
+
     let currentImage = 0;
     const slide = document.querySelector('#myimage');
+    const information = document.querySelector('#information');
 
     document.querySelector('#next').addEventListener('click', nextPhoto);
 
@@ -19,6 +22,8 @@
         }
 
         slide.src = `images/${myImages[currentImage]}`;
+        information.innerHTML = myInformation[currentImage];
+        
     }
 
     function previousPhoto(){
@@ -29,5 +34,6 @@
         }
 
         slide.src = `images/${myImages[currentImage]}`;
+        information.innerHTML = myInformation[currentImage];
     }
 }());
